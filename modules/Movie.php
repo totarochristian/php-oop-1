22318f92f5e809e2f274fed7d561fdc580efdbe7
totarochristian <totaro.christian.18@gmail.com>
2023-05-16 14:20:36 +0200

    Add attributes in the Movie class

--- modules/Movie.php
+++ modules/Movie.php
@@ -1,4 +1,10 @@
 <?php
 class Movie{
-  
+  private int $id;
+  private string $title;
+  private string $originalTitle;
+  private string $nationality;
+  private DateTime $date;
+  private float $vote;
+  private string $image;
 }
