8d4170c13498df430ec800917713dbd8ec025ab8
totarochristian <totaro.christian.18@gmail.com>
2023-05-16 14:28:34 +0200

    Add constructor with optional params

--- modules/Movie.php
+++ modules/Movie.php
@@ -7,4 +7,13 @@ class Movie{
   private DateTime $date;
   private float $vote;
   private string $image;
+  public function __construct($id=-1,$title="",$originalTitle="",$nationality="",$date="",$vote=0,$image=""){
+    $this->id = $id;
+    $this->title = $title;
+    $this->originalTitle = $originalTitle;
+    $this->nationality = $nationality;
+    $this->$date = $date ? getDate(strtotime($date)) : date("Y-m-d");
+    $this->vote = $vote;
+    $this->image = $image;
+  }
 }
