270a3cf7c89508f03d9c3a4e15ecb42dd4914000
totarochristian <totaro.christian.18@gmail.com>
2023-05-17 10:06:41 +0200

    Update the movieCard element to manage no results returned by the call of fetchMoviesFromDatabase

--- partials/template/moviesCard.php
+++ partials/template/moviesCard.php
@@ -2,9 +2,15 @@
   <?php
     //Gets movies array from the database
     $arrayOfMovies = Movie::fetchMoviesFromDatabase($conn);
-    //Call the method to print the movie data for each movie founded in the database
-    foreach($arrayOfMovies as $movie){
-      $movie->printMovieCard();
+    //If there are results, print data in the screen
+    if(count($arrayOfMovies)>0){
+      //Call the method to print the movie data for each movie founded in the database
+      foreach($arrayOfMovies as $movie){
+        $movie->printMovieCard();
+      }
+    }else{
+      echo "<h2>Siamo spiacenti, non sono stati trovati film da poter visualizzare</h2>";
     }
+    
   ?>
 </div>
